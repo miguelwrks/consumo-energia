fe3ba7c1068ad34b19feb1c72b2eaeab0e606eee
Miguel Silva Gonallves <312750021+miguelwrks@users.noreply.github.com>
2026-09-01 19:39:45 +0000

    feat - adicionado o calculo do cunsumo e do custo estimado

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear()
 print('--> Programa Consumo de energia! <--\n')
 
 aparelho = str(input("Digite o nome do aparelho: "))
@@ -5,6 +11,19 @@ aparelho = str(input("Digite o nome do aparelho: "))
 potencia = float(input('\nDigite a potência do aparelho em Watts (W): ')) #float pra n dar nenhumm problema
 
 usoHoras = float(input("\nDigite o tempo médio de uso diário do aparelho em horas: "))
+
+
+consumo = (usoHoras*30*(potencia))/1000
+
+#preço a pagar medio
+custo_estimado = (consumo)*0.75
+
+
+clear()
+print ('Aparelho --> ',aparelho)
+print('O consumo foi de -->',consumo, 'kWh/mes')
+print('O custo estimado e de --->', custo_estimado,'R$')
+print('Programa desenvolvido por ---> Miguel Silva Gonçalves')
 
 #calcular consumo mensal em kWh
 # consumo sendo: potencia multiplicado por horas ao dia vezes 30 (um mes) e dps tudo isso dividido por mil (para dar o k do kWh [quilo watt])
